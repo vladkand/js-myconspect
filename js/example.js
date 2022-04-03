f5bc2be91961cc355187fd2ef01123e2f91df89f
Vladimir K <chag120521@gmail.com>
2022-04-04 00:49:53 +0300

    online

--- js/example.js
+++ js/example.js
@@ -43,55 +43,10 @@ console.log(typeof valueC);//->number */
 // console.log(lastName);
 // ===============================
 
-// *prompt()
-
-// const hotelName = prompt("Please enter desired hotel name(название)");
-// console.log(hotelName);// -> "5"
-// console.log(`typeof всегда строка`,typeof hotelName);// ->string2
-// ==============================
-
-// * Перевод строки в число
-// let quantity = prompt('введите количество'); // если ввести буквы ->NaN
-// quantity = Number(quantity);
-// console.log(quantity);
-
-// console.log(typeof quantity);
-// ================================
-
-// let x = '12.2 px';
-// x = Number(x);
-// console.log(typeof x);//-> number
-// console.log(x); //-> NaN
-// ============================
-
-// * Проверка на число NaN
-// Number.isNaN(val)
-/* 
-const invalidNum = '52';
-console.log(isNaN(invalidNum)); 
-*/
-// const myNum = Number('52');
-// console.log(myNum);
-
-// function calculateTotalPrice (orderedQuantity, pricePerItem) {
-//   // Change code below this line
-
-//   const totalPrice = orderedQuantity *  pricePerItem;
-
-//   // Change code above this line
-//   return totalPrice;
-// };
 
 // console.log(Number(true));//->1
 
-//* логическое И '&&'
-let red;
-// console.log(red);
 
-//* логическое Или '||'
-
-// console.log(5 || 4 || 0); //-> 0
-// console.log(null || NaN || false); //-> false
 
 //* логическое И '!'
 
@@ -138,19 +93,5 @@ let red;
 // console.log(Math.floor(value)); // -> просто обрез после ","
 // console.log(Math.ceil(value)); // --> всегда целое(до запятой) в больш.
 // console.log(Math.round(value)); // - > как вматематике
-const ADMIN_PASSWORD = null;
-let message;
-// Change code below this line
-switch (ADMIN_PASSWORD) {
-  case null:
-    message = 'Canceled by user!';
-    breack;
 
-  case 'jqueryismyjam':
-    message = 'Welcome!';
-    breack;
 
-  default:
-    message = 'Access denied, wrong password!';
-}
-console.log(message);
